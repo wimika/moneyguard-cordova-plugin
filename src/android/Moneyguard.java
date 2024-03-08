da3d5e6253b3bdd3ae89a7e827079378e6ba308c
FrankDupree <frankyotana@yahoo.com>
2024-03-08 12:20:44 +0100

    returning session object

--- src/android/Moneyguard.java
+++ src/android/Moneyguard.java
@@ -1,4 +1,4 @@
-package com.wimika.ionic.moneyguard;
+package com.wimika.ionic.Moneyguard;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -6,65 +6,22 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import android.util.Log;
-
-
-/**
- * This class echoes a string called from JavaScript.
- */
-
- public interface IBasicSession {
-    String getInstallationId();
-    String getSessionId();
-}
-
-public class BasicSessionMock implements IBasicSession {
-    private String installationId;
-    private String sessionId;
-
-    public BasicSessionMock(String installationId, String sessionId) {
-        this.installationId = installationId;
-        this.sessionId = sessionId;
-    }
-
-    @Override
-    public String getInstallationId() {
-        return installationId;
-    }
-
-    @Override
-    public String getSessionId() {
-        return sessionId;
-    }
-}
+import com.wimika.moneyguard.Session;
 
 public class Moneyguard extends CordovaPlugin {
 
-
-    private IBasicSession registerGuard(String partnerBankId, String sessionToken) {
-        // Mock implementation using BasicSessionMock
-        return new BasicSessionMock(partnerBankId, sessionToken);
-    }
-
-    private JSONObject sessionToJson(IBasicSession session) {
-        JSONObject json = new JSONObject();
-        try {
-            json.put("InstallationId", session.getInstallationId());
-            json.put("SessionId", session.getSessionId());
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return json;
-    }
+  private Session session;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        
+
         Log.d("MoneyguardPlugin", "Executing " + action + " action");
         if (action.equals("Register")) {
             String partnerBankId = args.getString(0);
             String sessionToken = args.getString(1);
-            IBasicSession session = registerGuard(partnerBankId, sessionToken);
+            session = new SessionImpl();
             JSONObject sessionJson = sessionToJson(session);
             callbackContext.success(sessionJson);
             return true;
@@ -76,14 +33,11 @@ public class Moneyguard extends CordovaPlugin {
         return false;
     }
 
-    private void registerGuard(String partnerBankId, String sessionToken, CallbackContext callbackContext) {
-        // if (partnerBankId != null && partnerBankId.length() > 0
-        //         && sessionToken != null && sessionToken.length() > 0) {
-        //     MoneyGuardSdk.register(this, partnerBankId, sessionToken, null);
-        //     callbackContext.success("success integrating moneyguard");
-        // } else {
-        //     callbackContext.error("Expected one non-empty string argument.");
-        // }
+    private JSONObject sessionToJson(Session session) throws JSONException {
+        JSONObject sessionJson = new JSONObject();
+        sessionJson.put("SessionId", session.getSessionId());
+        // Convert other session properties to JSON as needed
+        return sessionJson;
     }
 
     private void echo(String message, CallbackContext callbackContext) {
